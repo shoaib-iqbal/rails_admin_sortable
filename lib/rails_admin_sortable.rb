30ebc1aa0354046b7578a0747bd755fcb5816b13
KOVACS Nicolas <perso.nkovacs@gmail.com>
2016-03-22 17:15:39 +0100

    suite

--- lib/rails_admin_sortable.rb
+++ lib/rails_admin_sortable.rb
@@ -15,8 +15,27 @@ module RailsAdmin
 
         register_instance_option :controller do
           Proc.new do
-            @sortable_section =  model_config.sortable
+            @sortable_section = model_config.sortable
+            @objects = list_entries.sort { |a,b| a.send(@sortable_section.position_method) <=> b.send(@sortable_section.position_method) }
+
+            if request.post?
+              original_model =  @abstract_model.model
+
+              params[:data].each do |key,value|
+                object = original_model.find(value[:id])
+                object.send("#{@sortable_section.position_method}=", value[:position])
+                object.save!
+              end
+
+              respond_to do |format|
+                format.json { render :json => { status: 'OK' }  }
+              end
+            end
           end
+        end
+
+        register_instance_option :visible? do
+          !bindings[:abstract_model].config.sortable.position_method.nil?
         end
 
         register_instance_option :object_level do
@@ -37,6 +56,10 @@ module RailsAdmin
 
         register_instance_option :pjax? do
           false
+        end
+
+        register_instance_option :http_methods do
+          [:get, :post, :put]
         end
 
       end
